bec85a52e9fffce96c7b2664fc78b089f11df6b8
ab380765597 <zhangxi0728@gmail.com>
2016-06-06 21:13:04 -0700

    Update YelpAPI.java

--- src/yelp/YelpAPI.java
+++ src/yelp/YelpAPI.java
@@ -17,13 +17,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.util.ToolRunner;
 
 import redis.clients.jedis.Jedis;
 
